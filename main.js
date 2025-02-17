8c6308add6feea80831ab3ea29a4013046f1be5e
mohumedali <midoaliii927@gmail.com>
2025-02-17 06:07:01 +0200

    update files

--- main.js
+++ main.js
@@ -8,3 +8,29 @@ const observer = new IntersectionObserver(entries => {
 }, { threshold: 0.3 });
 
 boxes.forEach(box => observer.observe(box));
+
+let arrow=document.getElementById('arrow');
+window.onscroll=function(){
+    if(scrollY >= 400)
+    {
+        arrow.style.opacity='1';
+        arrow.style.transition='0.4s';
+    }
+    else{
+        arrow.style.opacity='0'
+        
+    }
+}
+arrow.onclick=function(){
+    scroll({
+        behavior:"smooth",
+        top:'0'
+    })
+}
+let tech=document.getElementById('tech-skills')
+let btn_read=document.getElementById('btn-read')
+let soft=document.getElementById('soft-skills')
+btn_read.onclick=function(){
+    tech.style.display='none'
+    soft.style.display='block'
+}
